97638ba0428e15bcb714e0dba62ece141398ffb6
Tomás Carvalho <tomascarvalho7@gmail.com>
2024-06-09 21:31:34 +0100

    Fixed intent detector and search conflicts

--- modules/IntentDetector.py
+++ modules/IntentDetector.py
@@ -12,63 +12,20 @@ class IntentDetector:
                 id_to_intent[i] = intent
                 intent_to_id[intent] = i
 
-<<<<<<< HEAD
-        # Custom intent mapping
-        self.intent_mapping = {
-            "Greetings": "GreetingIntent",
-            "Search recipe": "QuestionIntent",
-            "Out of scope": "OutOfScopeIntent",
-            "Yes": "YesIntent",
-            "No": "NoIntent",
-            "Start task": "StartStepsIntent",
-            "Next": "NextStepIntent",
-            "Stop": "StopIntent"
-        }
-
-        # Reverse mapping
-        self.reverse_mapping = {v: k for k, v in self.intent_mapping.items()}
-        print(f"Reverse Mapping: {self.reverse_mapping}")
-
-        # Create id to intent and intent to id mappings based on the pre-trained model's space
-        id_to_intent, intent_to_id = dict(), dict()
-        for i, intent in enumerate(self.reverse_mapping.keys()):
-            id_to_intent[i] = intent
-            intent_to_id[intent] = i
-=======
->>>>>>> new-branch
 
         self.tokenizer = AutoTokenizer.from_pretrained("roberta-base")
         
         self.model = AutoModelForSequenceClassification.from_pretrained(
-<<<<<<< HEAD
-            model_name,
-            num_labels=len(self.reverse_mapping),
-            id2label=id_to_intent,
-            label2id=intent_to_id,
-            ignore_mismatched_sizes=True
-        )
-        self.tokenizer = AutoTokenizer.from_pretrained("roberta-base")
-=======
             "NOVA-vision-language/task-intent-detector",
             num_labels=len(all_intents), 
             id2label=id_to_intent, 
             label2id=intent_to_id
             )
->>>>>>> new-branch
 
     def detect_intent(self, text):
         model_in = self.tokenizer(text, return_tensors='pt')
         with torch.no_grad():
             logits = self.model(**model_in).logits
             predicted_class_id = logits.argmax().item()
-<<<<<<< HEAD
-            detected_intent = self.model.config.id2label[predicted_class_id]
-
-        print(f"Detected intent in model's label space: {detected_intent}")
-        mapped_intent = self.reverse_mapping.get(detected_intent, "Out of scope")
-        print(f"Mapped intent: {mapped_intent}")
-        return mapped_intent
-=======
             intent = self.model.config.id2label[predicted_class_id]
         return intent
->>>>>>> new-branch
--- modules/Search.py
+++ modules/Search.py
@@ -1,16 +1,7 @@
 import os
 from .EmbeddingUtils import EmbeddingUtils
 from opensearchpy import OpenSearch
-<<<<<<< HEAD
-from .SearchBuilder import SearchBuilder
-from .SlotFilling import SlotFilling
-from .IntentDetector import IntentDetector
-import torch
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
-import json
-=======
 from modules.SearchBuilder import SearchBuilder
->>>>>>> new-branch
 
 
 class Search:
@@ -31,49 +22,6 @@ class Search:
             ssl_assert_hostname=False,
             ssl_show_warn=False
         )
-<<<<<<< HEAD
-        # Initialize intent detector
-        self.intent_detector = IntentDetector("./all_intents.json", "NOVA-vision-language/task-intent-detector")
-
-        # Initialize slot filling
-        self.slot_filling = SlotFilling()
-
-        # Initialize state tracking
-        self.current_recipe_id = None
-        self.current_step = 0
-
-
-    def detect_intent(self, text):
-        return self.intent_detector.detect_intent(text)
-    
-    def get_recipe_by_id(self, recipe_id):
-        # Function to fetch the recipe by ID from the database
-        with open("../receitas.json", 'r') as f:
-            data = json.load(f)
-        return data.get(recipe_id, None)
-    
-    def get_step(self, recipe_id, step_number):
-        recipe = self.get_recipe_by_id(recipe_id)
-        if not recipe:
-            return "Recipe not found."
-        
-        instructions = recipe.get("instructions", [])
-        if 0 <= step_number < len(instructions):
-            step = instructions[step_number]
-            return f"Step {step['stepNumber']}: {step['stepText']}"
-        else:
-            return "Step not found."
-        
-    def get_next_step(self, recipe_id):
-        return self.get_step(recipe_id, self.current_step)
-        
-    def get_previous_step(self, recipe_id):
-        return self.get_step(recipe_id, self.current_step - 2)
-
-
-##### daqui para cima ####
-=======
->>>>>>> new-branch
 
     def SearchTitleAndDescriptionTxt(self, query):
         searchBuilder = SearchBuilder()
@@ -143,122 +91,4 @@ class Search:
 
     def SearchByCaptionEmbeddings(self, query):
         searchBuilder = SearchBuilder()
-<<<<<<< HEAD
         return searchBuilder.SearchByCaptionEmbeddings(query)
-    
-    def process_query(self, text):
-        intent = self.detect_intent(text)
-        print(f"Detected intent: {intent}")
-
-
-        if intent == 'Search recipe':
-            self.current_recipe_id = None
-            self.current_step = 0
-            return self.SearchTitleAndDescriptionTxt(text)
-        
-        elif intent == 'Next':
-            if self.current_recipe_id is not None:
-                self.current_step += 1
-                return self.get_next_step(self.current_recipe_id)
-            else:
-                return "You haven't started any recipe. Please search for a recipe first."
-            
-        elif intent == 'Yes':
-            return "Confirmed."
-        
-        elif intent == 'No':
-            return "Action canceled."
-        
-        ## aqui current step should be 0 or am i wrong ##
-        elif intent == 'Start task':
-            self.current_step = 1
-            if self.current_recipe_id is not None:
-                return self.get_next_step(self.current_recipe_id)
-            else:
-                return "You haven't started any recipe. Please search for a recipe first."
-            
-        elif intent == 'Stop':
-            self.current_recipe_id = None
-            self.current_step = 0
-            return "Stopping the task."
-        
-        elif intent == 'Greetings':
-            return "Hello! How can I assist you today?"
-        
-        elif intent == 'Out of scope':
-            return "I'm sorry, I cannot assist with that request."
-        
-        elif "previous" in text.lower():
-            if self.current_recipe_id is not None:
-                self.current_step -= 1
-                return self.get_previous_step(self.current_recipe_id)
-            else:
-                return "You haven't started any recipe. Please search for a recipe first."
-            
-        elif "jump to step" in text.lower():
-            step_number = int(text.split()[-1])
-            if self.current_recipe_id is not None:
-                self.current_step = step_number
-                return self.get_step(self.current_recipe_id, step_number - 1)
-            else:
-                return "You haven't started any recipe. Please search for a recipe first."
-            
-        elif "go two steps forward" in text.lower():
-            if self.current_recipe_id is not None:
-                self.current_step += 2
-                return self.get_next_step(self.current_recipe_id)
-            else:
-                return "You haven't started any recipe. Please search for a recipe first."
-            
-        elif "first step" in text.lower():
-            self.current_step = 1
-            if self.current_recipe_id is not None:
-                return self.get_step(self.current_recipe_id, 0)
-            else:
-                return "You haven't started any recipe. Please search for a recipe first."
-            
-        elif "last step" in text.lower():
-            if self.current_recipe_id is not None:
-                recipe = self.get_recipe_by_id(self.current_recipe_id)
-                if recipe:
-                    last_step = len(recipe.get("instructions", []))
-                    self.current_step = last_step
-                    return self.get_step(self.current_recipe_id, last_step - 1)
-                else:
-                    return "Recipe not found."
-            else:
-                return "You haven't started any recipe. Please search for a recipe first."
-            
-        elif "step number" in text.lower():
-            step_number = int(text.split()[-1])
-            if self.current_recipe_id is not None:
-                return self.get_step(self.current_recipe_id, step_number - 1)
-            else:
-                return "You haven't started any recipe. Please search for a recipe first."
-            
-        elif "name of the recipe" in text.lower():
-            if self.current_recipe_id is not None:
-                recipe = self.get_recipe_by_id(self.current_recipe_id)
-                if recipe:
-                    return f"The recipe is {recipe['displayName']}."
-                else:
-                    return "Recipe not found."
-            else:
-                return "You haven't started any recipe. Please search for a recipe first."
-            
-        elif "is it over" in text.lower():
-            instructions = recipe.get("instructions", [])
-            if self.current_step == len(instructions) -1:
-                return "I believe we have reached the end of the task. Remember, you can always come back to revisit an old recipe or start a new project. Have a wonderful day and happy cooking!"
-            else:
-                return "No"
-        elif "quit" in text.lower():
-            return "Goodbye!"
-        else:
-            return "I'm not sure how to handle that request."
-        
-    def extract_slot_value(self, question, context):
-        return self.slot_filling.extract_slot(question, context)
-=======
-        return searchBuilder.SearchByCaptionEmbeddings(query)
->>>>>>> new-branch
